1b30016712286f3bfcc59f2f8cf37ba07fa2c425
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2025-04-04 20:47:21 -0500

    Update 30_days_lost_in_space_day_16.ino
    minor updates (removed duplicate voids in front of functions) and updated n to N in showNumberDecEX and showNumberDec methods of hero_display

--- 30_days_lost_in_space_day_16.ino
+++ 30_days_lost_in_space_day_16.ino
@@ -12,17 +12,17 @@ const byte all_on[] = { 0b1111111,
                         0b1111111};
 
 const byte done[] = {
-    SEG_B | SEG_D | SEG_E | SEG_G,  // d  //0b0101101
+    SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,  // d  //0b0111101
     SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F, //O //0b1111110
     SEG_C | SEG_E | SEG_G,  //n //0b0010101
     SEG_A | SEG_D | SEG_E | SEG_F | SEG_G  //E //0b1001111
 };
 
-void void setup(){
+void setup(){
     hero_display.setBrightness(7); //configure brightness from 0-7
 }
 
-void void loop(){
+void loop(){
     hero_display.clear(); //clears hero_display
     delay(1000);
 
@@ -33,13 +33,13 @@ void void loop(){
     delay(1000);
 
     for (int i = 0; i < 4; i++){
-        hero_display.shownumberDecEx(1200,0b01000000);
+        hero_display.showNumberDecEx(1200,0b01000000);
         delay(500);
         hero_display.clear();
         delay(500);
     }
     for (int i = -100; i <= 100; i++){
-        hero_display.shownumberDec(i);
+        hero_display.showNumberDec(i);
         delay(50);
     } 
     delay(1000);
