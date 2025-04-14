3bd55d2a28a3819ed67acadec4e18096ff7ff9c5
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2025-04-14 16:20:09 -0500

    Update 30_days_lost_in_space_day_20_B.ino
    moved while loop to setup function so that it won't reset at the beginning of every loop in the while loop. 
    added updateEncoder() so the interrupt function will work with the encoder. 
    added the done array for testing on the 7 segment display.
    fixed minor syntax i think

--- 30_days_lost_in_space_day_20_B.ino
+++ 30_days_lost_in_space_day_20_B.ino
@@ -7,31 +7,46 @@
 const byte CLOCK_CLK_PIN = 6;
 const byte CLOCK_DIO_PIN = 5;
 
-const byte DIAL_CLK_PIN = 3;
-const byte DIAL_DT_PIN = 2;
+const byte DIAL_CLK_PIN = 2;
+const byte DIAL_DT_PIN = 3;
 const byte DIAL_SW_PIN = 4;
 
 BasicEncoder dial(DIAL_CLK_PIN, DIAL_DT_PIN);
 TM1637Display clock_face = TM1637Display(CLOCK_CLK_PIN, CLOCK_DIO_PIN);
+
+const byte done[] = {
+    SEG_B | SEG_C | SEG_D | SEG_E | SEG_G,  // d  //0b0111101
+    SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F, //O //0b1111110
+    SEG_C | SEG_E | SEG_G,  //n //0b0010101
+    SEG_A | SEG_D | SEG_E | SEG_F | SEG_G  //E //0b1001111
+};
 
 void setup(){
     clock_face.setBrightness(7);
     pinMode(DIAL_SW_PIN, INPUT);
     attachInterrupt(digitalPinToInterrupt(DIAL_CLK_PIN), updateEncoder, CHANGE);
     attachInterrupt(digitalPinToInterrupt(DIAL_DT_PIN), updateEncoder, CHANGE);
+    bool blink_12 = true;
+     while (blink_12) {
+        clock_face.showNumberDecEx(1200,0b01000000);
+        delay(500);
+        clock_face.clear();
+        delay(500);
+        
+        if (dial.get_change()){
+            blink_12 = false;
+            clock_face.setSegments(done);
+            delay(10000);
+        }
+    }
+    
 }
 
 void loop(){
-    bool blink_12 = true
-    while blink_12 {
-        clock_face.showNumberDecEx(1200,0b01000000);
-        delay(250);
-        clock_face.clear();
-        delay(250);
-    }
-    if (dial.get_change()){
-        blink_12 = false;
-    }
+    
+   
 
 }
-
+void updateEncoder(){
+    dial.service();
+}
