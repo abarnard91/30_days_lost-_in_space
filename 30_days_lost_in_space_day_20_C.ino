26be493af1712787fc34eb2cb49cc044fbca7b2c
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2025-05-04 15:52:59 +0000

    updated 20_C added library for arduino, keypad, and added the pin consts for the buzzer and keypad.

--- 30_days_lost_in_space_day_20_C.ino
+++ 30_days_lost_in_space_day_20_C.ino
@@ -1,1 +1,25 @@
 // Juke box using keypad and passive buzzer
+#include "Arduino.h";
+#include <Keypad.h>;
+
+const byte ROW_PINS[ROWS] = { 5, 4, 3, 2};
+const byte COL_PINS[COLS] = { 6, 7, 8, 13};
+
+const char BUTTONS[ROWS][COLS] = {
+  { '1', '2', '3', 'A'},
+  { '4', '5', '6', 'B'},
+  { '7', '8', '9', 'C'},
+  { '*', '0', '#', 'D'}
+};
+
+Keypad heroKeypad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
+
+const byte BUZZER_PIN = 12;
+
+void setup(){
+
+}
+
+void loop(){
+
+}
